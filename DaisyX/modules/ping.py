20e6fabfec56ac09af1bca1e970f42ef07db5821
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-05-21 16:24:47 +0800

    Update ping.py

--- DaisyX/modules/ping.py
+++ DaisyX/modules/ping.py
@@ -74,13 +74,13 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    message = msg.reply_text("Sabar Kontol...")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "PONG!!\n"
+        "MENGONTOLLL!!!\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
@@ -98,7 +98,7 @@ def ding(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     messageX.edit_text(
-        "! !DONG! !\n"
+        "! !NGENTOTTT! !\n"
         "<b>Time Taken:</b> <code>{}</code>\n"
         "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
