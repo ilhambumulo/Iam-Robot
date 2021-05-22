e4e171ff4694158165d865a12a31422aa1f15870
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-05-22 21:21:47 +0800

    Update fun_strings.py

--- DaisyX/modules/helper_funcs/fun_strings.py
+++ DaisyX/modules/helper_funcs/fun_strings.py
@@ -241,41 +241,45 @@ GDNIGHT = [
 
 # Taken from Openuserbot.
 GDMORNING = [
-    "`Life is full of uncertainties. But there will always be a sunrise after every sunset. Good morning!`",
-    "`It doesn’t matter how bad was your yesterday. Today, you are going to make it a good one. Wishing you a good morning!`",
-    "`If you want to gain health and beauty, you should wake up early. Good morning!`",
-    "`May this morning offer you new hope for life! May you be happy and enjoy every moment of it. Good morning!`",
-    "`May the sun shower you with blessings and prosperity in the days ahead. Good morning!`",
-    "`Every sunrise marks the rise of life over death, hope over despair and happiness over suffering. Wishing you a very enjoyable morning today!`",
-    "`Wake up and make yourself a part of this beautiful morning. A beautiful world is waiting outside your door. Have an enjoyable time!`",
-    "`Welcome this beautiful morning with a smile on your face. I hope you’ll have a great day today. Wishing you a very good morning!`",
-    "`You have been blessed with yet another day. What a wonderful way of welcoming the blessing with such a beautiful morning! Good morning to you!`",
-    "`Waking up in such a beautiful morning is a guaranty for a day that’s beyond amazing. I hope you’ll make the best of it. Good morning!`",
-    "`Nothing is more refreshing than a beautiful morning that calms your mind and gives you reasons to smile. Good morning! Wishing you a great day.`",
-    "`Another day has just started. Welcome the blessings of this beautiful morning. Rise and shine like you always do. Wishing you a wonderful morning!`",
-    "`Wake up like the sun every morning and light up the world your awesomeness. You have so many great things to achieve today. Good morning!`",
-    "`A new day has come with so many new opportunities for you. Grab them all and make the best out of your day. Here’s me wishing you a good morning!`",
-    "`The darkness of night has ended. A new sun is up there to guide you towards a life so bright and blissful. Good morning dear!`",
-    "`Wake up, have your cup of morning tea and let the morning wind freshen you up like a happiness pill. Wishing you a good morning and a good day ahead!`",
-    "`Sunrises are the best; enjoy a cup of coffee or tea with yourself because this day is yours, good morning! Have a wonderful day ahead.`",
-    "`A bad day will always have a good morning, hope all your worries are gone and everything you wish could find a place. Good morning!`",
-    "`A great end may not be decided but a good creative beginning can be planned and achieved. Good morning, have a productive day!`",
-    "`Having a sweet morning, a cup of coffee, a day with your loved ones is what sets your “Good Morning” have a nice day!`",
-    "`Anything can go wrong in the day but the morning has to be beautiful, so I am making sure your morning starts beautiful. Good morning!`",
-    "`Open your eyes with a smile, pray and thank god that you are waking up to a new beginning. Good morning!`",
-    "`Morning is not only sunrise but A Beautiful Miracle of God that defeats the darkness and spread light. Good Morning.`",
-    "`Life never gives you a second chance. So, enjoy every bit of it. Why not start with this beautiful morning. Good Morning!`",
-    "`If you want to gain health and beauty, you should wake up early. Good Morning!`",
-    "`Birds are singing sweet melodies and a gentle breeze is blowing through the trees, what a perfect morning to wake you up. Good morning!`",
-    "`This morning is so relaxing and beautiful that I really don’t want you to miss it in any way. So, wake up dear friend. A hearty good morning to you!`",
-    "`Mornings come with a blank canvas. Paint it as you like and call it a day. Wake up now and start creating your perfect day. Good morning!`",
-    "`Every morning brings you new hopes and new opportunities. Don’t miss any one of them while you’re sleeping. Good morning!`",
-    "`Start your day with solid determination and great attitude. You’re going to have a good day today. Good morning my friend!`",
-    "`Friendship is what makes life worth living. I want to thank you for being such a special friend of mine. Good morning to you!`",
-    "`A friend like you is pretty hard to come by in life. I must consider myself lucky enough to have you. Good morning. Wish you an amazing day ahead!`",
-    "`The more you count yourself as blessed, the more blessed you will be. Thank God for this beautiful morning and let friendship and love prevail this morning.`",
-    "`Wake up and sip a cup of loving friendship. Eat your heart out from a plate of hope. To top it up, a fork full of kindness and love. Enough for a happy good morning!`",
-    "`It is easy to imagine the world coming to an end. But it is difficult to imagine spending a day without my friends. Good morning.`",
+    "Hidup ini penuh dengan ketidakpastian. Tapi akan selalu ada matahari terbit setelah setiap matahari terbenam. Selamat pagi!`",
+
+    "Tidak peduli seberapa buruk Anda kemarin. Hari ini, Anda akan membuatnya menjadi hari yang baik. Selamat pagi!`",
+
+    "Jika ingin sehat dan cantik, sebaiknya bangun pagi. Selamat pagi sayang!",
+
+    "`Semoga pagi ini menawarkan harapan baru untuk hidup! Semoga Anda bahagia dan menikmati setiap momennya. Selamat pagi!",
+
+    "Semoga matahari menghujani kalian dengan berkah dan kemakmuran di hari-hari mendatang. Selamat pagi! ",
+
+    "`Setiap matahari terbit menandai kebangkitan kehidupan di atas kematian, harapan atas keputusasaan dan kebahagiaan atas penderitaan. Semoga pagi Anda menyenangkan hari ini!",
+
+    "`Bangun dan jadikan dirimu bagian dari pagi yang indah ini. Dunia yang indah sedang menunggu di luar pintumu. Selamat bersenang-senang!`",
+
+    "`Sambut pagi yang indah ini dengan senyuman di wajahmu. Semoga harimu menyenangkan hari ini. Semoga harimu menyenangkan!`",
+
+    "`Kamu telah diberkati dengan satu hari lagi. Betapa indahnya cara menyambut berkah dengan pagi yang begitu indah! Selamat pagi untukmu!`",
+
+    "`Bangun di pagi yang indah adalah jaminan untuk hari yang luar biasa. Saya harap Anda akan memanfaatkannya sebaik-baiknya. Selamat pagi!`",
+
+    "`Tidak ada yang lebih menyegarkan daripada pagi yang indah yang menenangkan pikiran Anda dan memberi Anda alasan untuk tersenyum. Selamat pagi! Semoga hari Anda menyenangkan.`",
+
+    "Hari lain baru saja dimulai. Sambutlah berkah pagi yang indah ini. Bangkit dan bersinar seperti yang selalu kau lakukan. Semoga pagimu indah!`",
+
+    "`Bangun seperti matahari setiap pagi dan nyalakan dunia kehebatanmu. Kamu memiliki begitu banyak hal besar untuk dicapai hari ini. Selamat pagi!`",
+
+    "`Hari baru telah datang dengan begitu banyak peluang baru untuk Anda. Raih semuanya dan manfaatkan hari Anda sebaik-baiknya. Selamat Pagi! Semoga Hari Anda Menyenangkan.`",
+
+    "`Kegelapan malam telah berakhir. Matahari baru telah terbit untuk membimbingmu menuju kehidupan yang begitu cerah dan bahagia. Selamat pagi sayang!",
+
+    "`Bangun, minumlah secangkir teh pagi dan biarkan angin pagi menyegarkanmu seperti pil kebahagiaan. Semoga kamu mendapatkan pagi yang baik kedepannya.`",
+
+    "`Matahari terbit adalah yang terbaik; nikmati secangkir kopi atau teh dengan diri Anda sendiri karena hari ini adalah milik Anda, selamat pagi! Semoga hari Anda menyenangkan.`",
+
+    "`Hari yang buruk akan selalu memiliki pagi yang baik, semoga semua kekhawatiran Anda hilang dan semua yang Anda inginkan dapat menemukan tempat. Selamat pagi!",
+
+    "`Akhir yang bagus mungkin belum diputuskan tetapi awal kreatif yang baik bisa menjadi rencana terbaik.` ",
+
+    "`Bangunlah dan tinggalkan mimpi Anda, Karena sekarang waktu Anda untuk menjadikannya nyata.",
 ]
 
 # Abuse strings credits @NotAMemeBot
